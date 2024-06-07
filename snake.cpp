1cdaed7c96705428116f8afbcc92f031650fda8f
“the <v54188abcde@outlook.com>
2024-06-08 07:23:00 +0800

    添加了食物

--- snake.cpp
+++ snake.cpp
@@ -2,6 +2,9 @@
 #include <stdio.h>
 #include <Windows.h>
 #include <graphics.h>
+#include <stdlib.h>
+#include <math.h>
+
 using namespace std;
 using namespace ege;
 
@@ -16,7 +19,7 @@ using namespace ege;
 #define GAME_WINDOW_Height 480 // 游戏窗口高度
 #define GAME_WINDOW_Title  "Snake Game" // 游戏窗口标题
 
-#define SNAKE_MAX_LEN      100          // 贪吃蛇的最大长度
+#define SNAKE_MAX_LEN      500          // 贪吃蛇的最大长度
 #define SNAKE_LEN          20                // 贪吃蛇的初始长度
 #define SNAKE_SPEED        1             // 贪吃蛇的速度一像素为单位
 #define SNAKE_WIDTH        20             // 贪吃蛇的宽度
@@ -29,11 +32,20 @@ using namespace ege;
 #define SNAKE_MOVE_UP      'W'   // 按键W
 #define SNAKE_MOVE_DOWN    'S'   // 按键S
 
+#define FOOD_COLOR         BGR(0, 255, 0) // 食物的颜色
+#define FOOD_WIDTH         20             // 食物的宽度
+
 typedef struct Snake_Node
 { // 贪吃蛇的节点结构体
     float x;
     float y;
 };
+typedef struct Food{  //食物的结构体
+    float x;
+    float y;
+    bool quite;
+};
+
 void swap_node(Snake_Node *a, Snake_Node *b){
     Snake_Node temp = *a;
     *a = *b;
@@ -49,6 +61,9 @@ bool GetKey(int Key){
 void cleanwindow(void){
     cleardevice();
 }
+int random(int min, int max){  //用于产生食物的随机数
+    return rand() % max + min;
+}
 void ExitGame(void){
     if(MessageBoxA(NULL, "Are you sure to exit?", "Exit", MB_OKCANCEL) == IDOK){
         exit(0);
@@ -57,6 +72,16 @@ void ExitGame(void){
         return;
     }
 }
+float distance_len(float x1, float y1, float x2, float y2){
+    return sqrt((x1 - x2) * (x1 - x2) + (y1 - y2) * (y1 - y2));
+}  //计算两个点的距离
+void draw_food(float x, float y){
+     for(int tx = x; tx < x + FOOD_WIDTH; tx++){
+            for(int ty = y; ty < y + FOOD_WIDTH; ty++){
+                putpixel(tx, ty, FOOD_COLOR);
+            }
+        }
+}  //画食物
 class SNAKE
 {
 public:
@@ -160,25 +185,57 @@ public:
             GameOver();
         }
     }
+    void PrintScore(){
+        char score_str[10];
+        sprintf(score_str, "%d", score);
+        outtextxy(GAME_WINDOW_Width - 100, 30, score_str); // 输出文字
+    }  //输出分数
+    void FastMove(void){
+        speed = 2;
+    }
 };
 class GAME{
     private:
         SNAKE snake;
+        Food food;
     public:
         void game(void){
             initgraph(GAME_WINDOW_Width, GAME_WINDOW_Height);// 初始化图形界面
             setcaption(GAME_WINDOW_Title);// 设置窗口标题
-
+            bool food_quite = true;  // 食物是否被吃掉(true表示未被吃掉)
+            food.x = random(0, GAME_WINDOW_Width - FOOD_WIDTH);  // 随机生成食物的坐标
+            food.y = random(0, GAME_WINDOW_Height - FOOD_WIDTH);
+            //food.quite = food_quite;
             while(true){
                 if(GetKey(VK_ESCAPE)){
                      ExitGame();
                 }
+                if(!food_quite){
+                    food_quite = true;  // 食物已被吃掉需要重新生成
+                }
+                if(food_quite)
+                {
+                    draw_food(food.x, food.y);  // 画食物
+                }
+                if(distance_len(snake.head->x, snake.head->y, food.x, food.y) < SNAKE_WIDTH){
+                   snake.score++;  // 吃到食物加分
+                   food_quite = false;  // 食物被吃掉
+                   snake.len++;  // 蛇长加1
+                   food.x = random(0, GAME_WINDOW_Width - FOOD_WIDTH);  // 重新生成食物
+                   food.y = random(0, GAME_WINDOW_Height - FOOD_WIDTH);
+                } // 食物与蛇头的距离小于蛇的宽度)
                 snake.Move();
                 snake.Draw();
-                //snake.Collision();
+                snake.Collision();
+                snake.PrintScore();//在右上角显示分数
+                if(GetKey(' ')){
+                    snake.FastMove();
+                }
+                else{
+                    snake.speed = 1;
+                }//按空格键加快游戏速度
                 delay(100); // 延时100毫秒
                 cleanwindow(); // 清除窗口
-                
             }
         }
 };
@@ -186,5 +243,6 @@ int main()
 {
     GAME game;
     game.game();
-   return 0;
+    
+    return 0;
 }
